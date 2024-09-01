3e9ecf7730f243c8bc67e8afe1c79a38895ad7f5
nevinmanoj <NEVINMANOJP@GMAIL.COM>
2024-09-02 00:38:28 +0530

    port update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 import routesMain from "./src/routes/routesMain.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(cors());
 app.use(express.json());
 
